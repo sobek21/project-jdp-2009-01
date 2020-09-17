0b0400d52eb356d5f0ac46ab18255db48c7762fc
draztel <poduszek.15@gmail.com>
2020-09-17 18:49:53 +0200

    UserController - wstepna implementacja endpointow

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,5 +1,6 @@
-package com.kodilla.ecommercee;
+package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.dto.user.UserDto;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
@@ -7,13 +8,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/v1")
 public class UserController {
     @RequestMapping(method = RequestMethod.POST, value = "createUser")
-    public void createUser(@RequestBody Object object) {
+    public void createUser(@RequestBody UserDto userDto) {
 
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "blockUser")
     public Object blockUser(@RequestParam Long userId) {
-        return new Object();
+        return new UserDto(1L, "blockedusername", "blockedfirstname", "blockedlastname");
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createUserKey")
---
+++ src/main/java/com/kodilla/ecommercee/dto/user/UserDto.java
@@ -0,0 +1,32 @@
+package com.kodilla.ecommercee.dto.user;
+
+public class UserDto {
+    private long userId;
+    private String username;
+    private String firstname;
+    private String lastname;
+
+    public UserDto(final long userId, final String username, final String firstname, final String lastname) {
+        this.userId = userId;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+}
