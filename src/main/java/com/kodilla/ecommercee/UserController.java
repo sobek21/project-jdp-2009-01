baaaee4056fbf951d06ab39b0003d22ebd5c0d2b
draztel <poduszek.15@gmail.com>
2020-09-17 12:49:55 +0200

    UserController - wstepna implementacja endpointow

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,23 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/v1")
 public class UserController {
+    @RequestMapping(method = RequestMethod.POST, value = "createUser")
+    public void createUser(@RequestBody Object object) {
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "blockUser")
+    public Object blockUser(@RequestParam Long userId) {
+        return new Object();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createUserKey")
+    public long createUserKey(@RequestParam Long userKey) {
+        return userKey;
+    }
 }
