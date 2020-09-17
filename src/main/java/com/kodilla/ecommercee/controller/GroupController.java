2e4585ec229d0d1a66362004837f12a3ac4c309e
Marcin <machikur2@gmail.com>
2020-09-17 17:30:28 +0100

    GroupController V1.0.1

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -1,33 +1,36 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.dto.group.GroupDto;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins ="*")
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/v1/groups")
 public class GroupController {
 
-    @GetMapping(value = "group")
-    public List<Object> getListOfAllGroups() {
-        return new ArrayList();
+    @GetMapping(value = "groups")
+    public List<GroupDto> getListOfAllGroups() {
+        List<GroupDto> list = new ArrayList<>();
+        list.add(new GroupDto(1, "test group"));
+        return list;
     }
 
     @GetMapping(value = "group")
-    public Object getGroupById(@RequestParam(value = "groupId") @NotNull Long groupId) {
-        return new Object();
+    public GroupDto getGroupById(@RequestParam(value = "groupId") @NotNull Long groupId) {
+        return new GroupDto(groupId, "test group");
     }
 
     @PostMapping(value = "group")
-    public void addNewGroup(@RequestBody Object object) {
+    public void addNewGroup(@RequestBody GroupDto groupDto) {
     }
 
     @PutMapping(value = "group")
-    public Object updateGroup(@RequestBody Object object) {
-        return object;
+    public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
+        return groupDto;
     }
 
 
