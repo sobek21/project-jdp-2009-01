a99c7d921bc5a630488f7f181d2c0acbce6f116a
sobek21 <mateuszsobkowicz211@gmail.com>
2020-09-21 20:02:29 +0200

    Cart Controller

--- src/main/java/com/kodilla/ecommercee/dto/CartDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/CartDto.java
@@ -1,4 +1,17 @@
 package com.kodilla.ecommercee.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class CartDto {
+
+    private Long cartId;
+    private Long userId;
+    private List<ProductDto> productDtoList;
 }
--- src/main/java/com/kodilla/ecommercee/dto/order/OrderDto.java
+++
@@ -1,4 +0,0 @@
-package com.kodilla.ecommercee.dto.order;
-
-public class OrderDto {
-}
