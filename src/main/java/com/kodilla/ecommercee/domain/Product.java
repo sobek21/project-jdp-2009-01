656a51d27d27099677a1072eb804636958c5c429
pablodlugi <dluskipawel13@gmail.com>
2020-09-23 21:43:40 +0200

    ProductEntity/ProductRepository/DbService

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,4 +1,27 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity(name = "PRODUCT")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long productId;
+
+    @Column(name = "NAME")
+    private String productName;
+
+    @Column(name = "PRICE")
+    private double productPrice;
+
+    @Column(name = "QUANTITY")
+    private int quantity;
 }
