f91aa5a7471f032b8698282e519c98468d7512d0
draztel <poduszek.15@gmail.com>
2020-09-24 16:11:25 +0200

    fixing UserDao

--- src/main/java/com/kodilla/ecommercee/repository/UserDao.java
+++ src/main/java/com/kodilla/ecommercee/repository/UserDao.java
@@ -2,7 +2,9 @@ package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserDao extends CrudRepository<User, Integer> {
     @Override
     User save(User Entity);
