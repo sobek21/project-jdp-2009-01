6450881de45c6881ffdba9ff808192558f88e326
sobek21 <mateuszsobkowicz211@gmail.com>
2020-09-25 16:11:49 +0200

    CartController

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,20 +1,18 @@
 package com.kodilla.ecommercee.controller;
 
 
-
 import com.kodilla.ecommercee.dto.CartDto;
 import com.kodilla.ecommercee.dto.OrderDto;
 import com.kodilla.ecommercee.dto.ProductDto;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1")
 public class CartController {
-
-
 
 
     @PostMapping("/createCart")
@@ -23,23 +21,27 @@ public class CartController {
     }
 
     @GetMapping("/getCart/{cartId}")
-    public CartDto getCart(@PathVariable Long cartId) {
-        return new CartDto();
+    public ProductDto getCart(@PathVariable int cartId) {
+        List<ProductDto> productDtoList = new ArrayList<ProductDto>();
+        productDtoList.add(new ProductDto((long) 0, "Test", 27, 1));
+        return productDtoList.get(cartId);
     }
 
     @PostMapping("/addProduct")
     public boolean addProduct(@RequestBody ProductDto productDto) {
-        return new CartDto().getProductDtoList().add(productDto);
+        List<ProductDto> productDtoList = new ArrayList<ProductDto>();
+        return productDtoList.add(productDto);
     }
 
     @DeleteMapping("/deleteProduct/{productId}")
-    public boolean deleteProduct(@PathVariable Long productId) {
-        return new CartDto().getProductDtoList().removeIf(element -> element.getProductId() == productId);
-
+    public ProductDto deleteProduct(@PathVariable int productId) {
+        List<ProductDto> productDtoList = new ArrayList<ProductDto>();
+        productDtoList.add(new ProductDto((long) 0, "Test", 27, 1));
+        return productDtoList.remove(productId);
     }
 
     @PostMapping("/createOrder")
     public OrderDto createOrder(@RequestBody OrderDto orderDto) {
-        return new OrderDto(1,"Name");
+        return new OrderDto(1, "Name");
     }
 }
