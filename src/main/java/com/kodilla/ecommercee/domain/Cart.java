08ca3a21a180da0e4e4b7b06a48bdcac3394442c
Marcin <machikur2@gmail.com>
2020-09-25 20:07:25 +0100

    Encja Cart Implementacja

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -1,4 +1,33 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "CART")
 public class Cart {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "CART_ID")
+    private long cartId;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "CART_PRODUCT",
+            joinColumns = {@JoinColumn(name = "CART_ID", referencedColumnName = "CART_ID")},
+            inverseJoinColumns = {@JoinColumn(name = "PRODUCT_ID", referencedColumnName = "PRODUCT_ID")}
+    )
+    private List<Product> products= new ArrayList<>();
+
 }
--- src/main/java/com/kodilla/ecommercee/repository/UserDao.java
+++ src/main/java/com/kodilla/ecommercee/repository/UserDao.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserDao extends CrudRepository<User, Integer> {
+public interface UserDao extends CrudRepository<User, Long> {
     @Override
     User save(User Entity);
 }
