67a442b6a72fcd8177ace8844ec57904f7498a5e
draztel <poduszek.15@gmail.com>
2020-09-30 12:35:24 +0200

    created relation in User

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,8 +1,15 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "USERS")
 public class User {
@@ -10,7 +17,7 @@ public class User {
     @Id
     @GeneratedValue
     @NotNull
-    @Column(name = "USER_ID", unique = true)
+    @Column(name = "USER_ID")
     private long userId;
 
     @Column(name = "USER_KEY")
@@ -27,5 +34,13 @@ public class User {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "cart_id", referencedColumnName = "cart_id")
     private Cart cart;
+
+    @OneToMany(
+            targetEntity = Order.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Order> orders = new ArrayList<>();
 
 }
--- src/main/java/com/kodilla/ecommercee/repository/UserDao.java
+++ src/main/java/com/kodilla/ecommercee/repository/UserDao.java
@@ -4,6 +4,9 @@ import com.kodilla.ecommercee.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Transactional
 @Repository
 public interface UserDao extends CrudRepository<User, Long> {
 
