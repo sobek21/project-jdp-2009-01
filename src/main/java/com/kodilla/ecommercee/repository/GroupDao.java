23c0781aaa3b4fdd5ae4faef4ef15adaea635645
draztel <poduszek.15@gmail.com>
2020-10-02 18:06:07 +0200

    naprawa relacji Group 1 - N Product

--- src/main/java/com/kodilla/ecommercee/repository/GroupDao.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupDao.java
@@ -2,6 +2,12 @@ package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
 public interface GroupDao extends CrudRepository <Group, Long> {
+
 }
