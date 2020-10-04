ef1f8e71464b2667da29e78126c6f7f337efc8eb
Marcin <machikur2@gmail.com>
2020-10-04 18:51:38 +0100

    conflicts fixed

--- src/main/java/com/kodilla/ecommercee/repository/GroupDao.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupDao.java
@@ -3,14 +3,10 @@ package com.kodilla.ecommercee.repository;
 import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-<<<<<<< HEAD
-import org.springframework.transaction.annotation.Transactional;
 
-=======
 
 import javax.transaction.Transactional;
 
->>>>>>> master
 @Transactional
 @Repository
 public interface GroupDao extends CrudRepository <Group, Long> {
